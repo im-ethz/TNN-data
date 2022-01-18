bbd94dd3386d9b1dfef1534b8fb24c61ac767954
evanweenen <evanweenen@ethz.ch>
2022-01-18 15:18:05 +0100

    Remove rider_mapping from config

---
+++ config.py
@@ -0,0 +1,9 @@
+import yaml
+
+with open('credentials.yml', 'r') as f:
+	credentials = yaml.safe_load(f) 
+
+with open('mapping.yml', 'r') as f:
+	rider_mapping = yaml.safe_load(f)
+
+rider_mapping_inv = {v:k for k,v in rider_mapping.items()}
